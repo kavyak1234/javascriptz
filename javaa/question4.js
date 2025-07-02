be1eaf675b77fe55922e9c47d8c25da148f73997
kavyak1234 <kavyakkavya153@gmail.com>
2025-07-02 17:26:57 +0530

    initial command

--- javaa/question4.js
+++ javaa/question4.js
@@ -1,3 +1,3 @@
-const numbers = [1,2,3,4];
-const doubled = numbers.map(num => num *2);
-console.log(doubled);
+// const numbers = [1,2,3,4];
+// const doubled = numbers.map(num => num *2);
+// console.log(doubled);
---
+++ javaa/question5.js
@@ -0,0 +1,3 @@
+const isEvenOROdd = num => num % 2 === 0 ? "Even" : "Odd";
+console.log(isEvenOROdd(4));
+console.log(isEvenOROdd(7));
