dc4e3824f2b39aada736a65765722acf8344344a
kavyak1234 <kavyakkavya153@gmail.com>
2025-07-02 17:39:59 +0530

    question6 is completed

---
+++ javaa/question6.js
@@ -0,0 +1,5 @@
+// const sayHi = name => Hi, ${name}!;
+// console.log(sayHi("kavya")); wrong  missing backticks(') around the string
+
+const sayHi = name => `Hi, ${name}!`;
+console.log(sayHi("kavya"));
